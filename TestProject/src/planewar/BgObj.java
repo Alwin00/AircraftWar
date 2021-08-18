e37446c7175e5ed5c43028e095e247066d9bf22a
XiaoQiang99168 <52444780+XiaoQiang99168@users.noreply.github.com>
2021-08-18 18:17:30 +0800

    eclipse2020
    JavaSE-15

---
+++ TestProject/src/planewar/BgObj.java
@@ -0,0 +1,22 @@
+package planewar;
+
+import java.awt.*;
+
+public class BgObj extends obj {
+    public BgObj() {
+        super();
+    }
+
+    public BgObj(Image img, int x, int y, double speed) {
+        super(img, x, y, speed);
+    }
+
+    @Override
+    public void paintSelf(Graphics gImage) {
+        super.paintSelf(gImage);
+        y += speed;
+        if (y >= 0){
+            y = -2000;
+        }
+    }
+}
